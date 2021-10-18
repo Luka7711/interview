da8a790f6353645145cff3c9e07a567fd68a292f
Luka7711 <35857649+Luka7711@users.noreply.github.com>
2021-10-18 12:45:27 -0500

    retrieving user

--- app.js
+++ app.js
@@ -4,38 +4,59 @@ const axios = require("axios");
 const PORT = 9000;
 const AWS = require("aws-sdk");
 
-const detailsByName = async (name) => {
-  let users = await axios.get("https://gorest.co.in/public/v1/users");
-  // find person by data
-  let foundUser = users.data.data.filter((user) => user.name === name);
-  console.log(foundUser);
-  return foundUser[0];
+const retrieveName = async (api) => {
+  const user = axios.get(api);
+  //search for user in result
+  // if found return user data
+  // else return retrieveName(next link)
 };
 
-const getCounts = async (name) => {
-  const allPosts = await axios.get("https://gorest.co.in/public/v1/posts");
-  const user = await detailsByName(name);
-  let postsByName = allPosts.data.data.filter(
-    (post) => post.user_id === user.id
-  );
-  if (!postsByName) console.log(`${name} has not posts`);
-  else console.log(`${name} HAS ${postsByName.length} POSTS`);
+let initialApi = "https://gorest.co.in/public/v1/users";
 
-  return postsByName;
+const detailsByName = async (name, api) => {
+  //make initial call
+  let userDetails = await axios.get(api);
+  let users = userDetails.data.data;
+  // search for user
+  if (users.length > 0) {
+    for (let user of users) {
+      if (user.name.toLowerCase() === name.toLowerCase()) {
+        console.log(user);
+        return user;
+      }
+    }
+  } else if (users.length === 0) {
+    console.log(null);
+    return null;
+  }
+  // if user not found search in next page
+  // if next page is exist call detailsByName
+  // else return foundUser
+  let nextApi = userDetails.data.meta.pagination.links.next;
+  return detailsByName(name, nextApi);
 };
 
-const recentPost = async (name) => {
-  const userPosts = await getCounts(name);
-  if (userPosts) console.log(userPosts[0]);
-  else console.log(`NO POSTS WAS RECORD BY ${name}`);
-};
-
-app.get("/", (req, res) => {
-  res.status(200).json({ name: "Luka" });
-});
+detailsByName("Mangalya Guha", initialApi);
 
 // recentPost("Nikita Ganaka");
 
+// const getCounts = async (name) => {
+//   const allPosts = await axios.get("https://gorest.co.in/public/v1/posts");
+//   const user = await detailsByName(name);
+//   let postsByName = allPosts.data.data.filter(
+//     (post) => post.user_id === user.id
+//   );
+//   if (!postsByName) console.log(`${name} has not posts`);
+//   else console.log(`${name} HAS ${postsByName.length} POSTS`);
+
+//   return postsByName;
+// };
+
+// const recentPost = async (name) => {
+//   const userPosts = await getCounts(name);
+//   if (userPosts) console.log(userPosts[0]);
+//   else console.log(`NO POSTS WAS RECORD BY ${name}`);
+// };
 app.listen(PORT, () => {
   console.log("App runs on port 9000");
 });
